b6ca5f2688a3bfe539ad7e998f6fcc1d52f6df9a
Rex Smith <rex.smith17@gmail.com>
2022-07-13 17:31:19 -0700

    Fixed concurrent creation and deletion for friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,20 +1,17 @@
 class FriendshipsController < ApplicationController
-  
+
   def destroy
     @friendship = Friendship.find(params[:id])
+    @inverse_friendship = Friendship.find_by(user_id: @friendship.friend_id, friend_id: @friendship.user_id)
     if @friendship.destroy
-      flash[:notice] = 'Friend successfully removed.'
-      redirect_to people_path
-    else
-      flash[:alert] = 'Something went wrong.'
-      redirect_to people_path
+      if @inverse_friendship.destroy
+        flash[:notice] = 'Friend successfully removed.'
+        redirect_to people_path
+      else
+        flash[:alert] = 'Something went wrong.'
+        redirect_to people_path
+      end
     end
-  end
-
-  private
-
-  def friendship_params
-    params.require(:friendship).permit(:friend_id)
   end
 
 end
